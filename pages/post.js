03b9965f1f98cbbc0f97fe940edc79d2d880979e
Damian <damiandanielvigo@gmail.com>
2021-03-10 22:09:39 -0300

    fix-router

--- pages/post.js
+++ pages/post.js
@@ -9,13 +9,15 @@ const Post = () => {
 
   const currentPost = posts.filter((post) => post.title === router.query.title)[0];
 
-  console.log(currentPost);
+  // console.log(currentPost);
 
   return (
     <Layout footer={false} title={router.query.title}>
       <div className='text-center'>
-        <img src={currentPost.imageURL} alt='' className='img-fluid' style={{ width: '50%' }} />
-        <p className='p-3 w-50 m-auto'>{currentPost.content}</p>
+        {router.query.title === 'React' || router.query.title === 'Svelte' || router.query.title === 'Vue' ? <img src={currentPost.imageURL} alt='' className='img-fluid' style={{ width: '50%' }} /> : ''
+        }
+        {router.query.title && <p className='p-3 w-50 m-auto'>{currentPost.content}</p>
+        }
       </div>
     </Layout>
   );
